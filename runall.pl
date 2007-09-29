ae9275169f625c1cfe4e849518a7be3e5dd4d592
Gabor Szabo <szabgab@gmail.com>
2007-09-29 19:28:27 +0000

    use Test::Harness 2.99_02 instead of the older TAP::Parser

--- runall.pl
+++ runall.pl
@@ -2,12 +2,20 @@
 use strict;
 use warnings FATAL => 'all';
 
-# requires perl and TAP::Parser 0.54 from http://search.cpan.org/dist/TAP-Parser/
 
 my $lua = "/usr/bin/lua";
 
 if (not @ARGV) {
     @ARGV = <t/*.lua>;
 }
-system("runtests --exec $lua @ARGV");
+
+# This version 
+# requires perl and TAP::Parser 0.54 from http://search.cpan.org/dist/TAP-Parser/
+#system("runtests --exec $lua @ARGV");
+#
+
+# This version
+# requires perl and Test::Harness http://search.cpan.org/dist/Test-Harness-2.99_02/
+# this is the preferred version now
+system("prove --exec $lua @ARGV");
 
